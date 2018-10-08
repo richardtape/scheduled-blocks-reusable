591c764ebabed5031eb1623d0d712af173f2db28
Richard Tape <richard.tape@ubc.ca>
2018-10-08 16:47:11 -0700

    Added software licensing and updates Added readme Added thank-yous Added license

--- scheduled-blocks-reusable.php
+++ scheduled-blocks-reusable.php
@@ -11,13 +11,17 @@
  * Plugin Name:  Scheduled Blocks Reusable Blocks Add-On
  * Plugin URI:   https://scheduledblocks.com/add-ons/reusable
  * Description:  Schedule when your reusable blocks go live. An add-on for Scheduled Blocks.
- * Version:      0.1.1
+ * Version:      0.1.2
  * Author:       Richard Tape
  * Requires PHP: 7
  * Author URI:   https://scheduledblocks.com/
  * Text Domain:  scheduled-blocks-reusable
- * License:      GPL-2.0+
- * License URI:  http://www.gnu.org/licenses/gpl-2.0.txt
+ * License:      GPL-3.0+
+ * License URI:  http://www.gnu.org/licenses/gpl-3.0.txt
+ * Icon1x:       https://raw.githubusercontent.com/froger-me/wp-plugin-update-server/master/examples/icon-128x128.png
+ * Icon2x:       https://raw.githubusercontent.com/froger-me/wp-plugin-update-server/master/examples/icon-256x256.png
+ * BannerHigh:   https://raw.githubusercontent.com/froger-me/wp-plugin-update-server/master/examples/banner-1544x500.png
+ * BannerLow:    https://raw.githubusercontent.com/froger-me/wp-plugin-update-server/master/examples/banner-722x250.png
  */
 
 // Exit if accessed directly.
@@ -26,7 +30,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Load our required files.
-require_once 'lib/class-scheduled-blocks-reusable.php';
+require_once plugin_dir_path( __FILE__ ) . 'lib/class-scheduled-blocks-reusable.php';
+require_once plugin_dir_path( __FILE__ ) . 'lib/wp-package-updater/class-wp-package-updater.php';
+
+$shceduled_blocks_reusable = new WP_Package_Updater(
+	'https://scheduledblocksdotcom.local',
+	wp_normalize_path( __FILE__ ),
+	wp_normalize_path( plugin_dir_path( __FILE__ ) ),
+	true
+);
 
 /**
  * Initialize ourselves!
@@ -35,8 +47,8 @@ require_once 'lib/class-scheduled-blocks-reusable.php';
  */
 function plugins_loaded__scheduled_blocks_reusable_init() {
 
-	$scheduled_blocks_reusable = new Scheduled_Blocks_Reusable();
-	$scheduled_blocks_reusable->init();
+	$scheduled_blocks_reusable_go = new Scheduled_Blocks_Reusable();
+	$scheduled_blocks_reusable_go->init();
 
 }// end plugins_loaded__scheduled_blocks_reusable_init()
 
